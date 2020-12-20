8d60711f22f3055a6aa4bb1480d5fb48412c6932
Jakeehmann42 <Jakeehmann42@gmail.com>
2020-12-20 14:09:16 -0700

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/budgetTracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
