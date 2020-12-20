e0b11148c5fb87c13df144afe239f996057f6347
Jakeehmann42 <Jakeehmann42@gmail.com>
2020-12-19 17:03:55 -0700

    added indexdb and connected

---
+++ public/db.js
@@ -0,0 +1,63 @@
+let db;
+const request = indexedDB.open("budget", 1);
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    if (navigator.online) {
+        checkDatabase();
+    }
+};
+
+
+
+
+
+
+request.onerror = function(event) {
+    console.log("Errrr! " + event.target.errorCode);
+};
+
+
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+}
+
+
+
+
+
+function checkdatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    const getAll =store.getAll();
+
+
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch ("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result);
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json" 
+                }
+            })
+
+
+
+            .then (response => response.json())
+            .then (() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store =transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    };
+}
+
+window.addEventListener("online", checkDatabase);
